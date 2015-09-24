3b8ade05e4ec69f106ba4c81834253c5e77955df
Jill Hickman <jillhickman@gmail.com>
2015-09-24 17:23:21 -0600

    Added picasso in the BookDetail, still need to add it to all other images

--- app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -18,9 +18,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import it.jaschke.alexandria.data.AlexandriaContract;
 import it.jaschke.alexandria.services.BookService;
-import it.jaschke.alexandria.services.DownloadImage;
 
 
 public class BookDetail extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -151,7 +152,10 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
-            new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
+//            new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
+            //Using Picasso to load images. Also added default placeholder image.
+            ImageView bookImageView = (ImageView) rootView.findViewById(R.id.fullBookCover);
+            Picasso.with(getActivity()).load(imgUrl).placeholder(R.drawable.ic_launcher).into(bookImageView);
             rootView.findViewById(R.id.fullBookCover).setVisibility(View.VISIBLE);
         }
 
